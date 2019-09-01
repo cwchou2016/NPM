e745c25323c0691dea239332a7d0ff83f5021934
MontyPan <monty.pan@dontcareabout.us>
2019-09-01 17:43:31 +0800

    Init by JISS

---
+++ src/main/java/us/dontcareabout/npm/client/NPMEP.java
@@ -0,0 +1,27 @@
+package us.dontcareabout.npm.client;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.user.client.Window;
+
+import us.dontcareabout.gwt.client.GFEP;
+
+public class NPMEP extends GFEP {
+	static final RpcServiceAsync rpc = GWT.create(RpcService.class);
+
+	public NPMEP() {}
+
+	@Override
+	protected String version() { return "0.0.1"; }
+
+	@Override
+	protected String defaultLocale() { return "zh_TW"; }
+
+	@Override
+	protected void featureFail() {
+		Window.alert("這個瀏覽器我不尬意，不給用..... \\囧/");
+	}
+
+	@Override
+	protected void start() {
+	}
+}
