8c4a1422015397dd010ed4fc20597090517bfe7a
cwchou2016 <s955401@gmail.com>
2019-12-08 14:26:45 +0800

    新增 RawData

---
+++ src/main/java/us/dontcareabout/npm/client/RawData.java
@@ -0,0 +1,40 @@
+package us.dontcareabout.npm.client;
+
+import us.dontcareabout.gwt.client.google.SheetEntry;
+
+import java.util.Date;
+
+public final class RawData extends SheetEntry {
+	protected RawData() {
+	}    //JSNI: JavaScriptObject 規格要求
+
+	public String getName() {
+		return stringField("name");
+	}
+
+	public String getRooms() {
+		return stringField("rooms");
+	}
+
+	public Date getStart() {
+		return dateField("start");
+	}
+
+	public Date getEnd() {
+		return dateField("end");
+	}
+
+	public boolean getClose() {
+		return stringField("close") == "TRUE" ? true : false;
+	}
+
+	public String display() {
+		return getName() + ", " +
+				getRooms() + ", " +
+				getClose() + ", " +
+				getStart() + ", " +
+				getEnd() + ", ";
+
+	}
+
+}
