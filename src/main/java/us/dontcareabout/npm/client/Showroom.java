84585f36d08b8770af8b95343c6308538eb7b48a
cwchou2016 <s955401@gmail.com>
2019-12-25 04:22:43 +0800

    新增 Showroom

---
+++ src/main/java/us/dontcareabout/npm/client/Showroom.java
@@ -0,0 +1,40 @@
+package us.dontcareabout.npm.client;
+
+/**
+ * 處理展廳
+ */
+public class Showroom {
+
+	/**
+	 * 第一個子展間名稱
+	 */
+	private static final char firstSubName = 'A';
+
+	/**
+	 * 判斷是否是子展間
+	 */
+	public static void isSubRoom(String room) {
+		//TODO
+	}
+
+	/**
+	 * 取得子展間的名稱
+	 */
+	public static void getSubName(String room) {
+		//TODO
+	}
+
+	/**
+	 * 取得母展間名稱
+	 */
+	public static void getParentName(String room) {
+		//TODO
+	}
+
+	/**
+	 * 將母展間切割為兩個子展間
+	 */
+	public static void splitRoom(String room) {
+		//TODO
+	}
+}
