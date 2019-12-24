26c5ab9917f8c899363fdb2606e968e53335981f
cwchou2016 <s955401@gmail.com>
2019-12-25 05:29:52 +0800

    完成 cutCloseInterval()

--- src/main/java/us/dontcareabout/npm/client/DateInfo.java
+++ src/main/java/us/dontcareabout/npm/client/DateInfo.java
@@ -23,8 +23,17 @@ public class DateInfo {
 	/**
 	 * 從展廳開放日期區間切除展廳暫時閉閉日期區間
 	 */
-	public void cutCloseInterval(DateInterval di) {
+	public void cutCloseInterval(DateInterval closeInterval) {
+		for (DateInterval dateInterval : openIntervals) {
+			if (dateInterval.contains(closeInterval)) {
+				// 這邊無法移動日期
+				addOpenInterval(closeInterval.getEnd(), dateInterval.getEnd()); //應為 closeInterval.getEnd() +1 天
+				dateInterval.setEnd(closeInterval.getStart()); // 應為 closeInterval.getStart() -1 天
+				return;
+			}
+		}
 
+		//TODO: 處理找不到可切除的日期區間的情形
 	}
 
 	public ArrayList<DateInterval> getOpenIntervals() {
